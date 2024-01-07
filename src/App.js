acf8bfcab0ea4693d6b59cb6d3fe611544632333
AB067240 <Abinash.Behera@cerner.com>
2024-01-07 20:21:42 +0530

    quick update to check jenkins pipeline

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Welcome to Film-Stack!</p>
+        <p>A place to see your movies collections :)</p>
       </header>
     </div>
   );
